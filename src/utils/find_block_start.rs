acaa2c7826a6c1a8a2f04047f6b2a574062ee327
Tomas Kanocz <tomas.kanocz@cnl.sk>
2020-05-12 22:02:27 +0200

    Fix tests for find_block_start

--- src/utils/find_block_start.rs
+++ src/utils/find_block_start.rs
@@ -28,24 +28,23 @@ mod test {
     fn test_find_block_start() {
         let data = &hex::decode("D9B4BEF9F9BEB4D9aabbccddeeff").unwrap();
         let (rest, chain) = find_block_start(data).unwrap();
-        println!("rest: {}", hex::encode(rest));
         assert_eq!(rest, &[0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff][..]);
-        assert_eq!(chain, Some("mainnet"));
+        assert_eq!(chain, Some(Network::Mainnet));
 
         let data = &hex::decode("01010100101010F9BEB4FEaabbccddeeff").unwrap();
         let (rest, chain) = find_block_start(data).unwrap();
         assert_eq!(rest, &[0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff][..]);
-        assert_eq!(chain, Some("namecoin"));
+        assert_eq!(chain, Some(Network::Namecoin));
 
         let data = &hex::decode("123124984011010101001010100B11090B110907aabbccddeeff").unwrap();
         let (rest, chain) = find_block_start(data).unwrap();
         assert_eq!(rest, &[0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff][..]);
-        assert_eq!(chain, Some("testnet"));
+        assert_eq!(chain, Some(Network::Testnet));
 
         let data = &hex::decode("010203040506070809101112FABFB5DAaabb").unwrap();
         let (rest, chain) = find_block_start(data).unwrap();
         assert_eq!(rest, &[0xaa, 0xbb][..]);
-        assert_eq!(chain, Some("regtest"));
+        assert_eq!(chain, Some(Network::Regtest));
 
         let data = &hex::decode("010101001010100709110Baabbccddeeff").unwrap();
         assert_eq!(true, find_block_start(data).is_err());
